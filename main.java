335ef93fde2c217084af31b580703cf85662138c
JeffreyChan0913 <53013457+JeffreyChan0913@users.noreply.github.com>
2021-09-09 22:00:55 -0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,67 +1,93 @@
-import java.util.Scanner;                                                                                    
-
-public class main
-
-{
-
-     public static void printName(String name)
-     {
-        int x = 0; 
-        String space = " ";
-        while(name.charAt(x) != space.charAt(0))
+import java.util.Scanner; 
+public class main{
+        final double milk = 3.99;
+	final double rice = 6.99;
+	final double orangeJuice = 5.99;
+	final double iceCream = 4.99;
+        String name; 
+        int option = 0;
+        double total = 0;
+        
+        public static void p(String str)
         {
-            x++;
+                System.out.println(str);
         }
-        System.out.println(name.substring(x));
-     }
-     public static void main(String[] args)
+        
+        public void add(final int option)
+        {
+                switch(option)
+                {
+                        case 1:
+                                this.total += this.milk;
+                                break;
+                        case 2:
+                                this.total += this.rice;
+                                break;
+                        case 3:
+                                this.total += this.orangeJuice;
+                                break;
+                        case 4:
+                                this.total += this.iceCream;
+                                break;
+                        default:
+                                p("wrong input, adding 0");
+                }
+        }
 
-     {
-         Scanner s = new Scanner(System.in);
-
-         int totalNumberOfStudents = 0, numberOfAs = 0, numberOfBs = 0, numberOfCs = 0, numberOfDs = 0, gradeHolder, cumGrade = 0;
-         String name;
-         char flag = 'c';
-         System.out.println("Enter your name");
-         name = s.nextLine();
-         do{
-             System.out.println("Enter a grade");
-
-             gradeHolder = s.nextInt(); 
-             cumGrade += gradeHolder;
-
-             if(gradeHolder >= 90)
-
-             {
-                 numberOfAs += 1;
-             }
-             else if(gradeHolder >= 80)
-             {
-                 numberOfBs += 1;
-             }
-             else if(gradeHolder >= 70)
-             {
-                 numberOfCs += 1;
-             }
-             else
-             {
-                 numberOfDs += 1; 
-             }
-
-             totalNumberOfStudents += 1; 
-             System.out.println("Enter q to quit, c to continue");
-             flag = s.next().charAt(0);  
-
-         }while(flag != 'q');
-
-         System.out.println("Number of A: " + numberOfAs);
-         System.out.println("Number of B: " + numberOfBs);
-         System.out.println("Number of C: " + numberOfCs);
-         System.out.println("Number of D: " + numberOfDs);
-         System.out.println("The average of this class is: " + cumGrade / totalNumberOfStudents);
-         System.out.println("Total number of students " + totalNumberOfStudents);
-         printName("Goodbye prof " + name);
-         s.close();
-
-     }
+        public void getName(Scanner s)
+        {
+                p("Enter your name");
+                String space = " ", comma = ",", str;
+                str = s.nextLine();
+                int pos = 0;
+                while(true)
+                {
+                        if(str.charAt(pos) == space.charAt(0))
+                        {
+                                this.name = str.substring(pos+1);
+                                break;
+                        }
+                        if(str.charAt(pos) == comma.charAt(0))
+                        {
+                                this.name = str.substring(0,pos);
+                                break;
+                        }
+                        pos++;
+                }
+        }
+        
+        public void finalize()
+        {
+                System.out.println("Your total: " + this.total);
+                System.out.println(this.name + " thank you for your business, good day ahead");
+        }
+       
+        public void menu()
+        {
+                Scanner s = new Scanner(System.in);
+                getName(s);
+                do
+                {
+                        p("1: Milk: 3.99");
+                        p("2: rice: 6.99");
+                        p("3: orange juice: 5.99");
+                        p("4: ice cream: 4.99");
+                        p("5: quit");
+                        p("Enter a number: ");
+                        this.option = s.nextInt();
+                        if(this.option != 5)
+                        {
+                                add(option);
+                        }
+                }while(this.option != 5);
+                s.close();
+                finalize();
+        }
+        
+        public static void main(String[] args)
+        {
+                main obj = new main();
+                obj.menu();
+        }
 }
+
